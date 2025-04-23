8e536b0c1832bd16bde540e06b804d4b155fb216
Santiago <Santiago@example.com>
2025-04-23 09:46:20 -0300

    Subo ejercicios falta el 4, 5 y 6 hacer el menu del 1

---
+++ ej Buscar x Nombre y Apellido.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+main(){
+		FILE *archivo;
+		char nombre[30], apellido[30];
+		char nombBusc[30], apellidoBusc[30];
+		int dni,i, cantidad, dniBusc; 
+		
+		archivo = fopen("usuarios.txt", "w");
+	
+		printf("Ingrese la cantidad de usuarios:");
+		scanf("%d", &cantidad);
+		getchar();
+
+		for(i=0; i<cantidad; i++){
+		printf("\nIngrese el Nombre: ");
+		fgets(nombre, sizeof(nombre), stdin);
+		nombre[strcspn(nombre, "\n")] = '\0';
+		printf("\nIngrese el Apellido: ");
+		fgets(apellido, sizeof(apellido), stdin);
+		apellido[strcspn(apellido, "\n")] = '\0';
+		printf("\nIngrese el DNI: ");
+		scanf("%d", &dni);
+		getchar();
+		fprintf(archivo, "Nombre %s\n", nombre);	
+		fprintf(archivo, "Apellido %s\n", apellido);
+		fprintf(archivo, "DNI %d\n", dni);
+		}
+		
+			
+		fclose(archivo);
+		
+		archivo = fopen("usuarios.txt", "r");
+		
+		printf("Ingrese el nombre de la persona que quiere buscar \n");
+		scanf("%s", nombBusc);
+		printf("Ingrese el apellido de la persona que quiere buscar \n");
+		scanf("%s", apellidoBusc);
+		
+		while (fscanf(archivo, "Nombre %s\nApellido %s\nDNI %d\n", nombre, apellido, &dni)==3){ 
+		if(strcmp(nombBusc, nombre) == 0 && strcmp(apellidoBusc, apellido) == 0){
+			printf("El nombre es %s y el apellido es %s, ", nombBusc, apellidoBusc);
+			printf("dni es %d \n", dni);
+		}
+	}
+		//printf("\nDatos guardados en el archivo usuario.txt");	
+		fclose(archivo);
+		
+	
+}
